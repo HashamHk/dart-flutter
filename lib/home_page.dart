8f15912978aa176bea5b91809ede75233abec347
HashamHk <60822100+HashamHk@users.noreply.github.com>
2020-11-05 10:57:01 +0500

    Secret Message App Done

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -9,28 +9,34 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<HomePage> {
-  var num1 = 0, num2 = 0, num3 = 0, randomNumber = 0, sum = 0;
+  var num1 = 0, num2 = 0, num3 = 0, sum = "";
+  int randomNumber;
   var messages = [
-    {1: "Hello there !"},
-    {2: "Working on Flutter"},
-    {3: "VS code is best"},
-    {4: "Waleed is installing Android SDK"},
-    {5: "It will take time to learn"},
-    {6: "Ye waqt bhi guzar jaey ga"},
-    {7: "Aa sajna tur chaliye"},
-    {8: "Bhola Records"},
-    {9: "Enough"},
-    {10: "End it"}
+    "Hello there !",
+    "Working on Flutter",
+    "VS code is best",
+    "Waleed is installing Android SDK",
+    "It will take time to learn",
+    "Ye waqt bhi guzar jaey ga",
+    "Aa sajna tur chaliye",
+    "Bhola Records",
+    "Enough",
+    "End it"
   ];
+  String secretMessage = "";
 
   final TextEditingController t1 = new TextEditingController(text: "0");
   final TextEditingController t2 = new TextEditingController(text: "0");
   final TextEditingController t3 = new TextEditingController(text: "0");
 
   void randomNumberGenerator() {
-    setState(() {
-      print(messages[1]);
-    });
+    random(min, max) {
+      var rn = new Random();
+      return min + rn.nextInt(max - min);
+    }
+
+    randomNumber = (random(0, 10));
+    print(randomNumber);
   }
 
   void compareInputsWithRandomNumber() {
@@ -39,13 +45,16 @@ class HomePageState extends State<HomePage> {
       num2 = int.parse(t2.text);
       num3 = int.parse(t3.text);
       if (num1 == randomNumber) {
-        print('num1 matched');
+        secretMessage = messages[randomNumber].toString();
+        print(secretMessage);
       } else if (num2 == randomNumber) {
-        print('num2 matched');
+        secretMessage = messages[randomNumber].toString();
+        print(secretMessage);
       } else if (num3 == randomNumber) {
-        print('num3 matched');
+        secretMessage = messages[randomNumber].toString();
+        print(secretMessage);
       } else {
-        return print('no number matched');
+        secretMessage = "No number matched";
       }
     });
   }
@@ -62,7 +71,7 @@ class HomePageState extends State<HomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             new Text(
-              "Output : $sum",
+              "Message : $secretMessage",
               style: new TextStyle(
                   fontSize: 20.0,
                   fontWeight: FontWeight.bold,
